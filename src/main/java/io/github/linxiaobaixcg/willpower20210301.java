68b5b9de6498b670805d6eb0bf133d9e36d7ce87
LinXiaoBaiXCG <1203718422@qq.com>
2021-03-01 23:04:13 +0800

    first

---
+++ src/main/java/io/github/linxiaobaixcg/willpower20210301.java
@@ -0,0 +1,36 @@
+package io.github.linxiaobaixcg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: daily-algorithm
+ * @description: 两数之和
+ * @author: LCQ
+ * @create: 2021-03-01 22:34
+ **/
+public class willpower20210301 {
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * 示例 1：
+     *
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
